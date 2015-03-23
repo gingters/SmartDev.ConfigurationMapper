9c66783795406ae8a7a3f44408ed4a5b1a914037
Sebastian P.R. Gingter <sebastian@gingter.org>
2015-03-23 22:51:45 +0100

    Added docs to the ConfigurationPropertyMapper methods

--- src/SmartDev.ConfigurationMapper/ConfigurationPropertyMapper.cs
+++ src/SmartDev.ConfigurationMapper/ConfigurationPropertyMapper.cs
@@ -1,21 +1,40 @@
 ﻿using System;
 using System.Reflection;
 using System.Globalization;
+
 using Microsoft.Framework.ConfigurationModel;
 
 namespace SmartDev.ConfigurationMapper
 {
+	/// <summary>
+	/// The <see cref="ConfigurationPropertyMapper">ConfigurationPropertyMapper</see> maps values from a given
+	/// <see cref="Microsoft.Framework.ConfigurationModel.IConfiguration">IConfiguration</see> instance to properties
+	/// of a given object.
+	/// </summary>
 	public class ConfigurationPropertyMapper
 	{
 		private IConfiguration _configuration;
 
+		/// <summary>
+		/// Creates a new instance of the <see cref="ConfigurationPropertyMapper">ConfigurationPropertyMapper</see>.
+		/// </summary>
+		/// <param name="configuration">An instance of
+		/// <see cref="Microsoft.Framework.ConfigurationModel.IConfiguration">IConfiguration</see>
+		/// to map values from.</param>
 		public ConfigurationPropertyMapper(IConfiguration configuration)
 		{
 			_configuration = configuration;
 		}
 
+		/// <summary>
+		/// Determines the properties of the given object, and maps the corresponding configuration values onto them.
+		/// </summary>
+		/// <param name="target">The object to map the configuration values to its properties.</param>
 		public void Map(object target)
 		{
+			if (target == null)
+				throw new ArgumentNullException("target");
+
 			// caution: GetProperties is a method on Type on normal CLR and this is an
 			// extension method in System.Reflection.TypeExtensions in CoreCLR
 			foreach (var propertyInfo in target.GetType().GetProperties())
