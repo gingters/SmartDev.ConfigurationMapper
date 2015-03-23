c31f0a61720ef64e8cfeacf4136c4a56e8b2a2a5
Sebastian P.R. Gingter <sebastian@gingter.org>
2015-03-23 23:08:54 +0100

    Added actual mapper with integration tests

---
+++ src/SmartDev.ConfigurationMapper/ConfigurationExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Microsoft.Framework.ConfigurationModel;
+
+namespace SmartDev.ConfigurationMapper
+{
+	/// <summary>
+	/// Provides extension methods for the <see cref="IConfiguration">IConfiguration</see> interface
+	/// to map configuration values to objects.
+	/// </summary>
+	public static class ConfigurationExtensions
+	{
+		/// <summary>
+		/// Maps values of the provided configuration to a new instance of the specified
+		/// configuration type.
+		/// </summary>
+		/// <typeparam name="T">The type of the configuration class to map to. Needs to
+		/// have a parameterless constructor.</typeparam>
+		/// <param name="configuration">An instance of <see cref="IConfiguration">IConfiguration</see>
+		/// to map values from.</param>
+		/// <returns>A new instance of <typeparamref name="T"/> filled with configuration values
+		/// matching the property names.</returns>
+		public static T Map<T>(this IConfiguration configuration)
+			where T : class, new()
+		{
+			if (configuration == null)
+				throw new ArgumentNullException("configuration");
+
+			var target = new T();
+			return configuration.Map(target);
+		}
+
+		/// <summary>
+		/// Maps values of the provided configuration to an existing instance of the specified
+		/// configuration type.
+		/// </summary>
+		/// <typeparam name="T">The type of the configuration class to map to. Usually can be
+		/// ommited.</typeparam>
+		/// <param name="configuration">An instance of <see cref="IConfiguration">IConfiguration</see>
+		/// to map values from.</param>
+		/// <param name="target">An existing object of type <typeparamref name="T"/>. Configuration
+		/// values will be mapped to this instance.</param>
+		/// <returns>The same object you passed in as <paramref name="target"/>, with the properties
+		/// mapped to the configuration values.</returns>
+		public static T Map<T>(this IConfiguration configuration, T target)
+			where T : class
+		{
+			if (configuration == null)
+				throw new ArgumentNullException("configuration");
+			if (target == null)
+				throw new ArgumentNullException("target");
+
+			var mapper = new ConfigurationPropertyMapper(configuration);
+			mapper.Map(target);
+
+			return target;
+		}
+	}
+}
---
+++ tests/ConfigurationMapper.IntegrationTests/ConfigurationExtensionTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Framework.ConfigurationModel;
+
+using Xunit;
+
+using SmartDev.ConfigurationMapper;
+
+namespace ConfigurationMapper.IntegrationTests
+{
+	public class ConfigurationExtensionTests
+	{
+		[Fact]
+		public void Map_Should_Instanciate_And_Fill_New_Object()
+		{
+			// arrange
+			var config = BuildSampleConfiguration();
+
+			// act
+			var obj = config.Map<SampleObject>();
+
+			// assert
+			Assert.Equal("Value1", obj.String1);
+			Assert.Equal(new DateTime(1979, 1, 17, 20, 59, 0), obj.Date1);
+			Assert.Null(obj.Key1);
+		}
+
+		[Fact]
+		public void Mapper_Should_Map_Values_To_Existing_Object()
+		{
+			// arrange
+			var config = BuildSampleConfiguration();
+			var obj = new SampleObject();
+
+			// act
+			config.Map(obj);
+
+			// assert
+			Assert.Equal("Value1", obj.String1);
+			Assert.Equal(new DateTime(1979, 1, 17, 20, 59, 0), obj.Date1);
+			Assert.Null(obj.Key1);
+		}
+
+		[Fact]
+		public void Mapper_Should_Pass_Existing_Object()
+		{
+			// arrange
+			var config = BuildSampleConfiguration();
+			var obj = new SampleObject();
+
+			// act
+			var obj2 = config.Map(obj);
+
+			// assert
+			Assert.Same(obj, obj2);
+			Assert.Equal("Value1", obj.String1);
+			Assert.Equal(new DateTime(1979, 1, 17, 20, 59, 0), obj.Date1);
+			Assert.Null(obj.Key1);
+		}
+
+		private class SampleObject
+		{
+			public string String1 { get; set; }
+			public DateTime Date1 { get; set; }
+			public string Key1 { get; set; }
+		}
+
+		private IConfiguration BuildSampleConfiguration()
+		{
+			var data = new Dictionary<string, string>()
+			{
+				{ "String1", "Value1" },
+				{ "Date1", "1979-01-17T20:59:00" },
+				{ "Scope1:Key1", "Scope1Value1" },
+				{ "Scope2:Key1", "Scope2Value1" },
+			};
+
+			var memoryConfigSource = new MemoryConfigurationSource(data);
+
+			var config = new Configuration();
+			config.Add(memoryConfigSource);
+
+			return config;
+		}
+	}
+}
